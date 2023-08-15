452b81513ef11a6f69f9b8cbabeb3d1db744515f
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2023-08-15 17:11:44 -0500

    c

---
+++ model.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import itertools
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, MaxPool2D, Conv2D, BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import RMSprop
+from keras.callbacks import ReduceLROnPlateau
+
+np.random.seed(1)
+#  Loading data
+train = pd.read_csv("data/train.csv")
+test = pd.read_csv("data/test.csv")
+
+# Adding labels
+Y_train = train['label']
+X_train = train.drop(labels=['label'], axis=1)
+
+X_train = X_train / 255.0
+test = test / 255.0
+X_train = X_train.values.reshape(-1, 28, 28, 1)
+test = test.values.reshape(-1, 28, 28, 1)
+Y_train = to_categorical(Y_train, num_classes=10)
+random_seed = 1
+X_train, X_val, Y_train, Y_val = train_test_split(
+    X_train, Y_train, test_size=0.1, random_state=random_seed)
+
+# Model
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(5, 5), padding='Same',
+          activation='relu', input_shape=(28, 28, 1)))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=32, kernel_size=(5, 5),
+          padding='Same', activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Conv2D(filters=64, kernel_size=(3, 3),
+          padding='Same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=64, kernel_size=(3, 3),
+          padding='Same', activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+model.summary()
+#  Compiling
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
+# Learning rate reduction
+learning_rate_reduction = ReduceLROnPlateau(
+    monitor='val_acc', patience=3, verbose=1, factor=0.5, min_lr=0.00001)
+# Tuning parameters
+epochs = 30
+batch_size = 64
+
+# Data Generator
+datagenerator = ImageDataGenerator(
+    featurewise_center=False,
+    samplewise_center=False,
+    featurewise_std_normalization=False,
+    samplewise_std_normalization=False,
+    zca_whitening=False,
+    rotation_range=0.1,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    horizontal_flip=False,
+    vertical_flip=False)
+datagenerator.fit(X_train)
+#
+history = model.fit_generator(datagenerator.flow(X_train, Y_train, batch_size=batch_size),
+                              epochs=epochs, validation_data=(X_val, Y_val),
+                              verbose=2, steps_per_epoch=X_train.shape[0] // batch_size,
+                              callbacks=[learning_rate_reduction])
+#
+results = model.predict(test)
+results = np.argmax(results, axis=1)
+results = pd.Series(results, name="Label")
+#
+submission = pd.concat(
+    [pd.Series(range(1, 28001), name="ImageId"), results], axis=1)
+submission.to_csv("data/submission.csv", index=False)
